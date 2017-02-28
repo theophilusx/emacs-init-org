858b2a37d42c97ef59b3c28688d615870e43de1c
Tim Cross <theophilusx@gmail.com>
2017-02-28 12:30:21 +1100

    Updated setup.el to ensure clean *.el generation

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # #       Filename: setup.sh
 # #  Creation Date: Saturday, 23 January 2016 01:02 PM AEDT
-# #  Last Modified: Saturday, 30 January 2016 11:00 AM AEDT
+# #  Last Modified: Tuesday, 28 February 2017 10:15 AM AEDT
 # #         Author: Tim Cross <theophilusx AT gmail.com>
 # #    Description:
 # #
 
 cd ~/.emacs.d
 
-/usr/local/bin/emacs -batch -l setup.el --eval "(tangle-my-init \"$*\")"
+echo "Tangle the file $*"
+
+/usr/local/bin/emacs -q -batch -l setup.el --eval "(tangle-my-init \"$*\")"
