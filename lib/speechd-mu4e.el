42f36fc713da1947502cb0ccf60e68777fcd8e13
Tim Cross <theophilusx@gmail.com>
2019-08-02 17:37:53 +1000

    More tweaks to speechd config

--- lib/speechd-mu4e.el
+++ lib/speechd-mu4e.el
@@ -5,11 +5,46 @@
 
 ;;; Code:
 (require 'mu4e)
+(require 'speechd-out)
 
-(defun speechd-init-mu4e ()
-  "Speech enable mu4e."
-  (speechd-speak-command-feedback mu4e-headers-next after (speechd-speak-read-line))
-  (speechd-speak-command-feedback mu4e-headers-prev after (speechd-speak-read-line)))
+(defun speechd-mu4e-msg-status (flags)
+  (mapconcat (lambda (e)
+               (case e
+                 ('draft "draft message")
+                 ('flagged "which is starred")
+                 ('new "new message")
+                 ('seen "seen message")
+                 ('passed ", has been forwarded")
+                 ('replied "you have replied to")
+                 ('trashed "and is marked deleted")
+                 ('attach "with attachments")
+                 ('encrypted ", is encrypted")
+                 ('signed ", is signed")
+                 ('unread "unread"))) flags " "))
+
+(defun speechd-mu4e-speak-msg-header ()
+  (let* ((mail-list (mu4e-message-field-at-point :mailing-list))
+         (from-1 (first (mu4e-message-field-at-point :from)))
+         (from (concat (if (listp from-1)
+                            (first from-1)
+                         from-1)
+                       (if mail-list
+                           (format " in list %s" mail-list)
+                         "")))
+         (subject (mu4e-message-field-at-point :subject))
+         (date (format-time-string mu4e-headers-date-format
+                                   (mu4e-message-field-at-point :date)))
+         (flags (speechd-mu4e-msg-status
+                 (mu4e-message-field-at-point :flags))))
+    (speechd-out-text (format "%s from %s subject %s on %s"
+                              flags from subject date)
+                      :priority 'text)))
+
+(speechd-speak-command-feedback mu4e-headers-next after
+                                (speechd-mu4e-speak-msg-header))
+
+(speechd-speak-command-feedback mu4e-headers-prev after
+                                (speechd-mu4e-speak-msg-header))
 
 (provide 'speechd-mu4e)
 ;;; speechd-mu4e.el ends here
